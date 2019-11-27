1018487e4f006aebc8400ca5222e6bb9b2624e34
ns954613 <1104104353@nkust.edu.tw>
2019-11-27 18:13:23 +0800

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular';
+  name = 'Angular Pratice 1';
+  
+  courses = [
+    { id: 1, name: 'course1'},
+    { id: 2, name: 'course2'},
+    { id: 3, name: 'course3'},
+  ];
+
+  onAdd() {
+    this.courses.push({id: 4, name: 'course4'});
+  }
+  onRemove(course) {
+    let index = this.courses.indexOf(course);
+    this.courses.splice(index, 1);
+  }
+  onChange(course) {
+    course.name = 'Updata course!';
+  }
+
+  viewMode = 'map';
 }
